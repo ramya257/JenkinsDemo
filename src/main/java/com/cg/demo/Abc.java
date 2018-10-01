6c031f3e721980d0933df4f6e854cb3911e21801
ramya257 <educate1996@gmail.com>
2018-10-01 13:45:54 +0530

    added new class

--- src/main/java/com/cg/demo/Abc.java
+++ src/main/java/com/cg/demo/Abc.java
@@ -2,7 +2,11 @@ package com.cg.demo;
 
 public class Abc {
 
+	
 	public static void main(String[] args){
+		Bcd bcd=new Bcd();
+		
+		bcd.getDisplay("Linking classes");
 		System.out.println("Hello World");
 	}
 }
---
+++ src/main/java/com/cg/demo/Bcd.java
@@ -0,0 +1,9 @@
+package com.cg.demo;
+
+public class Bcd {
+
+	
+	public void getDisplay(String message){
+		System.out.println(message);
+	}
+}
